b5bc0932a84bb63ba9f3f38fa80666e2b8117694
SmartEggAnn <815177551@qq.com>
2017-06-07 19:02:07 +0800

    springboot，前端在 src\main\resources\static，kernel放在controller同一层。用springboot跑了之后默认进入mainpage.html

---
+++ src/main/java/com/bi/Controller/DefaultViewController.java
@@ -0,0 +1,20 @@
+package com.bi.Controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class DefaultViewController {
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName(
+                        "forward:/mainpage.html");
+            }
+        };
+    }
+}
